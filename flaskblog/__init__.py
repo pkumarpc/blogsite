1ca16464f72d50b72d39f57b8867e95eeeaff736
Panneerchelvam, Premkumar <Premkumar.Panneerchelvam@kla-tencor.com>
2021-07-07 13:28:26 -0500

    Basic user authentication system

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -1,9 +1,15 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '3f5887cb8f7b86a60481d0b2d78b3dd4'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
 
 from flaskblog import routes
